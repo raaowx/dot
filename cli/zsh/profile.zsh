968e650d24b4497ed2ae4f75c3cb9e6b4666492b
raaowx <raaowx@protonmail.com>
2021-12-07 21:53:29 +0100

    [CLI] Unable to perform `scp`

--- cli/zsh/profile.zsh
+++ cli/zsh/profile.zsh
@@ -1,4 +1,5 @@
 # === === === ZSH PROFILE === === === #
+[ -z "$PS1" ] && return # Skip profile sourcing for not interative shells
 SHELL_PROFILE="$HOME/.shell_profile"
 if [ -f "$SHELL_PROFILE" ]; then
   . "$SHELL_PROFILE"
